8fa83ecd2a6c13f2124194622d55a913e2182191
jneis116 <73029023+jneis116@users.noreply.github.com>
2023-02-05 20:03:40 +0000

    Added ex5 from textbook as main.

--- xinu-x86-vm/system/main.c
+++ xinu-x86-vm/system/main.c
@@ -1,23 +1,49 @@
-/*  main.c  - main */
+/* ex5.c - main, prod2 cons2 */
 
 #include <xinu.h>
-#include <stdio.h>
+void prod2(sid32, sid32), cons2(sid32, sid32);
 
-int main(int argc, char **argv)
+int32 n = 0; /* Variable n has inital value zero */
+
+/*-------------------------------------------------------------------
+* main - Producer and Consumer processes synchronized with semaphores
+*--------------------------------------------------------------------
+*/
+void main(void)
 {
-	uint32 retval;
-
-	resume(create(shell, 8192, 50, "shell", 1, CONSOLE));
-
-	/* Wait for shell to exit and recreate it */
-
-	recvclr();
-	while (TRUE) {
-		retval = receive();
-		kprintf("\n\n\rMain process recreating shell\n\n\r");
-		resume(create(shell, 4096, 1, "shell", 1, CONSOLE));
-	}
-	while (1);
-
-	return OK;
+sid32 produced, consumed;
+consumed = semcreate(0);
+produced = semcreate(1);
+resume( create(cons2, 1024, 20, "cons", 2, consumed, produced) );
+resume( create(prod2, 1024, 20, "prod", 2, consumed, produced) );
 }
+
+/*-------------------------------------------------------------------------
+* prod2 - Increment n 2000 times, waiting for it to be consumed
+*---------------------------------------------------------------------------
+*/
+void prod2(sid32 consumed, sid32 produced)
+{
+int32 i;
+for(i=1; i<=2000; i++)
+{
+wait(consumed);
+n++;
+signal(produced);
+}
+}
+
+/*-------------------------------------------------------------------------
+* cons2 - Print n 2000 times, waiting for it to be produced
+*---------------------------------------------------------------------------
+*/
+void cons2(sid32 consumed, sid32 produced)
+{
+int32 i;
+for(i=1; i<=2000; i++)
+{
+wait(produced);
+printf("n is %d \n", n);
+signal(consumed);
+}
+
