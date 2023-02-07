84281dfe8b524aefc5f00f23388cce3d41141ee5
jneis116 <73029023+jneis116@users.noreply.github.com>
2023-02-07 02:33:40 +0000

    1st attempt

--- xinu-x86-vm/shell/xsh_create.c
+++ xinu-x86-vm/shell/xsh_create.c
@@ -8,37 +8,17 @@ xsh_create - shell command to create a new process with a specific priority
 --------------------------------------------------------------------------
 */
 
-shellcmd xsh_create(int nargs, char *args[]){
-    pid32	pid;			/* ID of the new process	   */
-    pri16 priority;           /* priority of the new process */
-    char	ch;			    /* next character of argument	*/
-	char	*chptr;			/* walks along argument string	*/
-    if (nargs>2){
-        kprintf("\nCreate command takes [1] argument, the priority of the new process.\n");
-        return 0;
-    }
-    if(nargs==1){
-        priority = 20;
-    }
-    if(nargs==2){
-        // char* to int taken from xsh_kill
-        chptr = args[1];
-        ch = *chptr++;
-        priority = 0;
-        while(ch != NULLCH) {
-            if ( (ch < '0') || (ch > '9') ) {
-                kprintf(stderr, "\n%s: non-digit in priotrity\n",
-                    args[0]);
-                return 1;
-            }
-            priority = 10*priority + (ch - '0');
-            ch = *chptr++;
-        }
-        
-    }
-    // kprintf("\nCreating process with priority %d\n", priority);
-    pid = createInfinite(priority);
-    kprintf("\npid: %d\n",pid);
-    resume(pid);
+shellcmd xsh_create(int nargs, char *args[])
+{
+     int num, modulo;
+     num = args[1];
+     modulo = num % 2;
+
+     if (modulo == 0)
+     {
+        printf("Number is even");
+     }
+    else printf("Number is odd");
+    
     return 1;
 }
