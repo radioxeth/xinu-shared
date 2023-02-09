7ca4df3327724478402c4cfff69984842edbd0c7
jneis116 <73029023+jneis116@users.noreply.github.com>
2023-02-09 00:26:00 +0000

    1st attempt, modified producer and consumer

--- xinu-x86-vm/system/main.c
+++ xinu-x86-vm/system/main.c
@@ -3,6 +3,7 @@
 #include <xinu.h>
 void prod2(sid32, sid32), cons2(sid32, sid32);
 
+sid32 sem;
 int32 n = 0; /* Variable n has inital value zero */
 int buffer[15]; // global array to act as buffer
 
@@ -12,6 +13,9 @@ int buffer[15]; // global array to act as buffer
 */ 
 void main(void)
 {
+	// Initiate the semaphore to 14 (size of the buffer -1)
+	sem = semcreate(14);
+
 	sid32 produced, consumed;
 	consumed = semcreate(0);
 	produced = semcreate(1);
@@ -20,31 +24,32 @@ void main(void)
 }
 
 /*-------------------------------------------------------------------------
-* prod2 - Increment n 2000 times, waiting for it to be consumed
+* prod2 - add to buffer and signal in loop from 1 to 15
 *---------------------------------------------------------------------------
 */
 void prod2(sid32 consumed, sid32 produced)
 {
 int32 i;
-	for(i=1; i<=2000; i++)
+	for(i=1; i<=15; i++)
 	{
 		wait(consumed);
+		buffer[i-1] = i;
 		n++;
 		signal(produced);
 	}
 }
 
 /*-------------------------------------------------------------------------
-* cons2 - Print n 2000 times, waiting for it to be produced
+* cons2 - wait for buffer to be full (sem = -1) then print buffer
 *---------------------------------------------------------------------------
 */
 void cons2(sid32 consumed, sid32 produced)
 {
 	int32 i;
-	for(i=1; i<=2000; i++)
+	for(i=15; i>0; i--)
 	{
 		wait(produced);
-		printf("n is %d \n", n);
+		printf("Buffer space 1 = %d \n", buffer[i-1]);
 		signal(consumed);
 	}
 }
