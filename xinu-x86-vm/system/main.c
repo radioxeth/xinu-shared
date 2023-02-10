2bc26d078b6166fd89a1132fa2a72053bb7ddcd8
jneis116 <73029023+jneis116@users.noreply.github.com>
2023-02-10 16:38:55 +0000

    Switched consume to 14, produce to 0

--- xinu-x86-vm/system/main.c
+++ xinu-x86-vm/system/main.c
@@ -17,8 +17,8 @@ void main(void)
 	sem = semcreate(14);
 
 	sid32 produced, consumed;
-	consumed = semcreate(0);
-	produced = semcreate(14);
+	consumed = semcreate(14);
+	produced = semcreate(0);
 	resume( create(cons2, 1024, 20, "cons", 2, consumed, produced) );
 	resume( create(prod2, 1024, 20, "prod", 2, consumed, produced) );
 }
