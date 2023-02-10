075803fe6bcca7564f83fb6112495e5e41ed6654
jneis116 <73029023+jneis116@users.noreply.github.com>
2023-02-10 16:18:44 +0000

    1st attempt 2 semaphores

--- xinu-x86-vm/system/main.c
+++ xinu-x86-vm/system/main.c
@@ -1,23 +1,55 @@
-/*  main.c  - main */
+/* ex5.c - main, prod2 cons2 */
 
 #include <xinu.h>
-#include <stdio.h>
+void prod2(sid32, sid32), cons2(sid32, sid32);
 
-int main(int argc, char **argv)
+sid32 sem;
+int32 n = 0; /* Variable n has inital value zero */
+int buffer[15]; // global array to act as buffer
+
+/*-------------------------------------------------------------------
+* main - Producer and Consumer processes synchronized with semaphores
+*--------------------------------------------------------------------
+*/ 
+void main(void)
 {
-	uint32 retval;
+	// Initiate the semaphore to 14 (size of the buffer -1)
+	sem = semcreate(14);
 
-	resume(create(shell, 8192, 50, "shell", 1, CONSOLE));
+	sid32 produced, consumed;
+	consumed = semcreate(0);
+	produced = semcreate(1);
+	resume( create(cons2, 1024, 20, "cons", 2, consumed, produced) );
+	resume( create(prod2, 1024, 20, "prod", 2, consumed, produced) );
+}
 
-	/* Wait for shell to exit and recreate it */
-
-	recvclr();
-	while (TRUE) {
-		retval = receive();
-		kprintf("\n\n\rMain process recreating shell\n\n\r");
-		resume(create(shell, 4096, 1, "shell", 1, CONSOLE));
+/*-------------------------------------------------------------------------
+* prod2 - add to buffer and signal in loop from 1 to 15
+*---------------------------------------------------------------------------
+*/
+void prod2(sid32 consumed, sid32 produced)
+{
+int32 i;
+	for(i=1; i<=15; i++)
+	{
+		wait(consumed);
+		buffer[i-1] = i;
+		n++;
+		signal(produced);
 	}
-	while (1);
+}
 
-	return OK;
+/*-------------------------------------------------------------------------
+* cons2 - wait for buffer to be full (sem = -1) then print buffer
+*---------------------------------------------------------------------------
+*/
+void cons2(sid32 consumed, sid32 produced)
+{
+	int32 i;
+	for(i=15; i>0; i--)
+	{
+		wait(produced);
+		printf("Buffer space 1 = %d \n", buffer[i-1]);
+		signal(consumed);
+	}
 }
