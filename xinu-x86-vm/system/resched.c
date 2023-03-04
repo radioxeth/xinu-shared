e6f2eb810bb5f3cb53b1dca3acd8d28ce0bc7f33
sameul3045 <123433023+sameul3045@users.noreply.github.com>
2023-03-04 14:40:55 -0700

    Update resched.c

--- xinu-x86-vm/system/resched.c
+++ xinu-x86-vm/system/resched.c
@@ -1,19 +1,23 @@
 /* resched.c - resched */
 
 #include <xinu.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /*------------------------------------------------------------------------
  *  resched  -  Reschedule processor to highest priority eligible process
  *------------------------------------------------------------------------
  */
-void	resched(void)		/* assumes interrupts are disabled	*/
+void resched(void) /* assumes interrupts are disabled	*/
 {
-	struct procent *ptold;	/* ptr to table entry for old process	*/
-	struct procent *ptnew;	/* ptr to table entry for new process	*/
+	struct procent* ptold; /* ptr to table entry for old process	*/
+	struct procent* ptnew; /* ptr to table entry for new process	*/
 
 	/* If rescheduling is deferred, record attempt and return */
 
-	if (Defer.ndefers > 0) {
+	if (Defer.ndefers > 0)
+	{
 		Defer.attempt = TRUE;
 		return;
 	}
@@ -22,8 +26,17 @@ void	resched(void)		/* assumes interrupts are disabled	*/
 
 	ptold = &proctab[currpid];
 
-	if (ptold->prstate == PR_CURR) {  /* process remains running */
-		if (ptold->prprio > firstkey(readylist)) {
+	if (ptold->prstate == PR_CURR)
+	{
+		/* process remains running */
+
+		if (g_xinuSchedulerType == SCHED_FCFS && ptold->prprio > 0)
+		{
+			return;
+		}
+
+		if (ptold->prprio > firstkey(readylist))
+		{
 			return;
 		}
 
@@ -38,7 +51,7 @@ void	resched(void)		/* assumes interrupts are disabled	*/
 	currpid = dequeue(readylist);
 	ptnew = &proctab[currpid];
 	ptnew->prstate = PR_CURR;
-	preempt = QUANTUM;		/* reset time slice for process	*/
+	preempt = QUANTUM; /* reset time slice for process	*/
 	ctxsw(&ptold->prstkptr, &ptnew->prstkptr);
 
 	/* Old process returns here when resumed */
